ba70787e16f8eaae40fe5afd3ac64bcae339135d
hydrz <n.haoyuan@gmail.com>
2019-11-24 01:06:37 +0800

    run_as_root

--- install-helm.sh
+++ install-helm.sh
@@ -16,8 +16,8 @@ install_helm() {
 	mkdir -p ${HELM_TMP}
 	wget -O ${TMP_ROOT}/helm.tar.gz ${HELM_DOWNLOAD_URL}/$HELM_VERSION/helm-${HELM_VERSION}-${OS}-${ARCH}.tar.gz
 	tar xf ${TMP_ROOT}/helm.tar.gz -C "${HELM_TMP}"
-	run_as_root "cp ${HELM_TMP}/${OS}-${ARCH}/helm ${BIN_INSTALL_DIR}/helm"
-	run_as_root "chmod +x ${BIN_INSTALL_DIR}/helm"
+	run_as_root cp ${HELM_TMP}/${OS}-${ARCH}/helm ${BIN_INSTALL_DIR}/helm
+	run_as_root chmod +x ${BIN_INSTALL_DIR}/helm
 
 	cat <<-EOF >${HELM_TMP}/helm-rbac.yaml
 		apiVersion: v1
@@ -42,7 +42,7 @@ install_helm() {
 	kubectl apply -f ${HELM_TMP}/helm-rbac.yaml
 	helm init --upgrade --service-account tiller -i registry.aliyuncs.com/google_containers/tiller:${HELM_VERSION} \
 		--stable-repo-url ${HELM_STABLE_REPO_URL}
-	run_as_root 'helm completion bash >/etc/bash_completion.d/helm'
+	run_as_root helm completion bash >/etc/bash_completion.d/helm
 }
 
 install_helm
