5a475a42c7e8aac3130153106a6c0631921f9920
hydrz <n.haoyuan@gmail.com>
2019-11-24 01:02:07 +0800

    kubelet.service.d

--- install-aliyun-cloud.sh
+++ install-aliyun-cloud.sh
@@ -24,6 +24,10 @@ install_aliyun_cloud() {
     META_EP=http://100.100.100.200/latest/meta-data
     REGION_ID=$(curl -s $META_EP/region-id)
     INSTANCE_ID=$(curl -s $META_EP/instance-id)
+    cat <<-EOF >/usr/lib/systemd/system/kubelet.service.d/20-aliyun.conf
+		[Service]
+		Environment="KUBELET_EXTRA_ARGS=--cloud-provider=external --hostname-override=${REGION_ID}.${INSTANCE_ID} --provider-id=${REGION_ID}.${INSTANCE_ID}"
+	EOF
 
     cat ${KUBEADM_INIT_CONFIG} |
         sed "s?name:.*?name: ${REGION_ID}.${INSTANCE_ID}?" |
@@ -32,42 +36,43 @@ install_aliyun_cloud() {
     CA_DATA=$(cat /etc/kubernetes/pki/ca.crt | base64 -w 0)
 
     cat <<-EOF | kubectl apply -f -
-    apiVersion: v1
-    data:
-        special.keyid: $ACCESS_KEY_ID
-        special.keysecret: $ACCESS_KEY_SECRET
-    kind: ConfigMap
-    metadata:
-        name: cloud-config
-        namespace: kube-system
-    ---
-    apiVersion: v1
-    kind: ConfigMap
-    metadata:
-      name: cloud-controller-manager
-      namespace: kube-system
-    data:
-      cloud-controller-manager.conf: |-
-        kind: Config
-        contexts:
-        - context:
-            cluster: kubernetes
-            user: system:cloud-controller-manager
-            name: system:cloud-controller-manager@kubernetes
-        current-context: system:cloud-controller-manager@kubernetes
-        users:
-        - name: system:cloud-controller-manager
-            user:
-            tokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
-        apiVersion: v1
-        clusters:
-        - cluster:
-            certificate-authority-data: $CA_DATA
-            server: $(k cluster-info | xargs -n 1 | grep http | head -1)
-            name: kubernetes
-EOF
+    	apiVersion: v1
+    	data:
+    	    special.keyid: $ACCESS_KEY_ID
+    	    special.keysecret: $ACCESS_KEY_SECRET
+    	kind: ConfigMap
+    	metadata:
+    	    name: cloud-config
+    	    namespace: kube-system
+	EOF
 
-    curl -sfL https://raw.githubusercontent.com/kubernetes/cloud-provider-alibaba-cloud/master/docs/examples/cloud-controller-manager.yml |
+    cat <<-EOF >/etc/kubernetes/cloud-controller-manager.conf
+		apiVersion: v1
+		kind: ConfigMap
+		metadata:
+		  name: cloud-controller-manager
+		  namespace: kube-system
+		data:
+			kind: Config
+			contexts:
+			- context:
+			    cluster: kubernetes
+			    user: system:cloud-controller-manager
+			    name: system:cloud-controller-manager@kubernetes
+			current-context: system:cloud-controller-manager@kubernetes
+			users:
+			- name: system:cloud-controller-manager
+			    user:
+			    tokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+			apiVersion: v1
+			clusters:
+			- cluster:
+			    certificate-authority-data: $CA_DATA
+			    server: $(k cluster-info | xargs -n 1 | grep http | head -1)
+			    name: kubernetes
+	EOF
+
+    cat ${PWD}/yaml/cloud-controller-manager.yml |
         sed "s?\${CLUSTER_CIDR}?${CLUSTER_CIDR}?" |
         kubectl apply -f -
 
